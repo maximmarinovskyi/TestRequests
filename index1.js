fe982b93472947a302d35fd148eb63798efa2d64
MaxxTheWebDeveloper <max0936003333@gmail.com>
2023-02-20 22:03:45 +0200

    comcom

--- index1.js
+++ index1.js
@@ -1,1 +1,11 @@
-console.log(1)
+const xhr = new XMLHttpRequest();
+
+xhr.open("get", "https://jsonplaceholder.typicode.com/todos/1", true)
+
+xhr.onload = () => {
+    console.log(xhr.readyState)
+    console.log(xhr.response)
+}
+
+xhr.send()
+
