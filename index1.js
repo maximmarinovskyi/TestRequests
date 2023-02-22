fb6df1df218fd2fe3c84b5b1dd7d90a6b42a5085
MaxxTheWebDeveloper <max0936003333@gmail.com>
2023-02-22 19:42:43 +0200

    123456789

--- index1.js
+++ index1.js
@@ -1,14 +1,21 @@
-const xhr = new XMLHttpRequest();
+const xhr1 = new XMLHttpRequest();
+const xhr2 = new XMLHttpRequest();
+const xhr3 = new XMLHttpRequest();
 
-xhr.open("get", "https://jsonplaceholder.typicode.com/todos/1", true)
+xhr1.open("get", "https://jsonplaceholder.typicode.com/todos/1", true)
 
-xhr.onload = () => {
-    console.log(xhr.readyState)
-    console.log(xhr.response)
+xhr1.onload = () => {
+    console.log(`xhr 1 readystste condition is: ${xhr1.readyState}`)
+    console.log(`xhr 1 readystste condition is: ${xhr1.response}`)
 }
 
-xhr.send()
+xhr1.send()
+////////////////////////////////////////////////////////////////////
+xhr2.open("get", "https://jsonplaceholder.typicode.com/posts/1")
 
-console.log(`Fuck...`)
-console.log(`Fuck...`)
-console.log(`Fuck...`)
+xhr2.onload = () => {
+    console.log(`xhr 2 readystste condition is: ${xhr2.readyState}`)
+    console.log(`xhr 2 readystste condition is: ${xhr2.response}`)
+}
+
+xhr2.send()
