2c6f409614f2d087739948159f727a6a753e80a6
MaxxTheWebDeveloper <max0936003333@gmail.com>
2023-02-23 19:56:41 +0200

    DOwnloading from github

--- index1.js
+++ index1.js
@@ -1,55 +1,17 @@
-<<<<<<< HEAD
-const xhr1 = new XMLHttpRequest();
-const xhr2 = new XMLHttpRequest();
-const xhr3 = new XMLHttpRequest();
+const smileOne = document.querySelector("header")
+const smileTwo = document.querySelector("main")
+const smileThree = document.querySelector("footer")
 
-xhr1.open("get", "https://jsonplaceholder.typicode.com/todos/1", true)
+const button1 = document.querySelector(".button1")
+const button2 = document.querySelector(".button2")
+const button3 = document.querySelector(".button3")
 
-xhr1.onload = () => {
-    console.log(`xhr 1 readystste condition is: ${xhr1.readyState}`)
-    console.log(`xhr 1 readystste condition is: ${xhr1.response}`)
-}
-
-xhr1.send()
-////////////////////////////////////////////////////////////////////
-xhr2.open("get", "https://jsonplaceholder.typicode.com/posts/1")
-
-xhr2.onload = () => {
-    console.log(`xhr 2 readystste condition is: ${xhr2.readyState}`)
-    console.log(`xhr 2 readystste condition is: ${xhr2.response}`)
-}
-
-xhr2.send()
-=======
-let smileOne = document.querySelector(".smile_one")
-let smileTwo = document.querySelector(".smile_one")
-let smileThree = document.querySelector(".smile_one")
-
-let header = document.querySelector("header")
-let main = document.querySelector("main")
-let footer = document.querySelector("footer")
-
-smileOne.addEventListener("click", () => {
-    randInt(0, 255)
-    changeColorHeader()
+button1.addEventListener("click", () => {
+    smileOne.classList.add("green")
 })
-smileTwo.addEventListener("click", changeColorMain)
-smileThree.addEventListener("click", changeColorFooter)
-
-function changeColorHeader() {
-    this.style.backgroundColor = `rgb(${randInt(0, 255)}, ${randInt(0, 255)}, ${randInt(0, 255)})`
-}
-function changeColorMain() {
-    this.style.backgroundColor = `rgb(${randInt(0, 255)}, ${randInt(0, 255)}, ${randInt(0, 255)})`
-}
-function changeColorFooter() {
-    this.style.backgroundColor = `rgb(${randInt(0, 255)}, ${randInt(0, 255)}, ${randInt(0, 255)})`
-}
-
-function randInt(min, max) {
-    let rand = min + Math.random() * (max + 1 - min)
-    return Math.floor(rand)
-}
-
-console.log(`/////////////////////////////////////////////////////////////////////////`)
->>>>>>> 4df11b950418f8d1acf38bf908f4d0cd0d15970c
+button2.addEventListener("click", () => {
+    smileTwo.classList.add("blue")
+})
+button3.addEventListener("click", () => {
+    smileTwo.classList.toggle("blue")
+})
